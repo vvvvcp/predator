8da4eab0b312c4b187cfba4a683fdd3b467b538d
hu xiao <huxiao0816@gmail.com>
2015-08-13 18:11:02 +0800

    update

---
+++ src/demobrain.py
@@ -0,0 +1,9 @@
+import pybrain
+from pybrain.tests.unittests.test_nested_network import buildNestedNetwork
+
+
+
+if __name__ == '__main__':
+    fnn = buildNestedNetwork()
+    print "huxiao add"
+    print fnn
